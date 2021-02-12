22f04f893c251692e1a95ee7cc894f19ea0c8538
Akelly <akelly982@hotmail.com>
2021-02-12 13:59:32 +1100

    just a quick init get some folders and files in place
    Basics for a website so its got the index page and some includes to help push us in the right direction and set up some base script connections within the header.

---
+++ webRestaurantDate/index.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <?php
+            include "modularContent/head/header.php";
+        ?>
+    </head>
+    
+    <navigation>
+        <?php
+            include "modularContent/nav/navigation.php"
+        ?>
+    </navigation>
+    
+    <body>
+        
+        <h2>This is some text</h2>
+        <p> show content here...... <p>
+    
+    </body>
+    
+    
+    
+    <footer>
+        <?php
+            include "modularContent/footer/footer.php"
+        ?>
+    </footer>
+
+</html>
---
+++ webRestaurantDate/modularContent/footer/footer.php
@@ -0,0 +1,4 @@
+<!doctype html>
+<html lang="en">
+    <h2> I am the Footer </h2>
+</html>
