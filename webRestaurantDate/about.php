a3f0eea57928c6a555b454be84309b1a7980c1ac
Igor Silva <79127562+Igorfsilvas@users.noreply.github.com>
2021-03-23 10:31:18 +1100

    Create about.php

---
+++ webRestaurantDate/about.php
@@ -0,0 +1,16 @@
+<!-- Begin: Get Header -->
+<?php
+    /* Name of page title */
+    $title = "Foodies";
+    /* Get header.php */
+    include "modularContent/header.php";
+?>
+<!-- Finish: Get Header -->
+
+<!-- Begin: Get Header -->
+<?php
+    include "modularContent/nav.php"
+?>
+<!-- Finish: Get Header -->
+
+
