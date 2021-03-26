aa89c1c7b9a944d3ff7c6feaa6a5b728c845ae39
Igor Silva <79127562+Igorfsilvas@users.noreply.github.com>
2021-03-27 09:36:23 +1100

    About page - Igor
    Looks like working with scss

--- webRestaurantDate/about.php
+++ webRestaurantDate/about.php
@@ -13,4 +13,49 @@
 ?>
 <!-- Finish: Get Header -->
 
+<!-- START: About page -->
+<div class="about_top">
+    <div class="about_box">
+        <h2>Food bla bla.......</h2>
+            <p>t is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose injected humour and the like </p>
+    </div>
+</div>
 
+<!-- We need to rename the class (Igor)-->
+<section>
+        <div class="home_restJoin">
+            <div class="home_restJoin_text">
+                <h2>Promoting your restaurant</h2>
+                    <div class="home_restJoin_text_block">
+                        <p>If you are restaurant owner, create your page for promoting your business. You can have a restaurant page for FREE!</p>
+                        <p>Additionally, users reviews could be great benefit for your business.</p>
+                    </div>
+                <div class="btn_black">
+                    <a href="">Create your page</a>
+                </div>
+            </div>        
+            <div class="home_restJoin_img"></div>
+        </div>
+    </section>
+
+
+
+<!-- END: Page Contents -->
+
+
+<!-- Begin: Get Footer -->
+<?php
+    /* Get footer.php */
+    include "modularContent/footer.php"
+?>
+<!-- Finish: Get Footer -->
+
+<!-- Begin: Connect to the Firebase  -->
+<?php
+    include "modularContent/firebaseInit.php";  
+?>
+
+    <!-- javascript for the page that we are on -->
+    <script src="js/firebaseUser.js"></script>
+    <script src="js/pgJavascript/home.js"></script>
+<!-- Finish: Connect to the Firebase  -->
